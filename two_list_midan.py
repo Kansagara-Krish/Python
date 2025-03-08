78f7125d8684b21224b9887fa3ab711e8d37defc
Krish_2006 <149057889+Kansagara-Krish@users.noreply.github.com>
2025-03-08 20:01:47 +0530

    Update two_list_midan.py

--- two_list_midan.py
+++ two_list_midan.py
@@ -7,23 +7,24 @@ The overall run time complexity should be O(log (m+n)).
 This code calculates the median of two sorted arrays by merging them into one array, sorting it, and identifying the middle element(s). It's useful for statistical analysis when combining datasets to find their central tendency.
 '''
 def median(arr1,arr2,m,n):
-    arr1.sort()
-    arr2.sort()
     arr3=[]
     arr3.extend(arr1)
     arr3.extend(arr2)
+    arr3.sort()
     print(arr3)
     if len(arr3)%2!=0:
-        mid=(len(arr3)//2)+1 
-        print("Median is",mid)
+        index=(len(arr3)//2)
+        mid=arr3[index]
+        if (-10**6) <=mid and mid<= (10**6):
+            print("Sorted combine list is",arr3,"Median is",mid)
     else:
         mid_1=(len(arr3)//2)
         mid_2=(len(arr3)//2)+1 
         if (-10**6) <=mid_1 and mid_2<= (10**6): 
             mid=(mid_1+mid_2)/2 
-            print("Midian is",mid)
+            print("Sorted combine list is",arr3,"Midian is",mid)
 
-arr1=[1,2]
+arr1=[10,2]
 arr2=[3,4]
 n=len(arr1)
 m=len(arr2)
